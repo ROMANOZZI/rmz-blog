30de8266f28822b1be8d1dfb7dab0c0d2143fb7e
ROMANOZZI <mohamedsalah2162000@gmail.com>
2023-09-13 06:43:00 +0300

    feed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const feed = (posts) => {
-  return <h1>posts</h1>;
+  return (
+    <>
+      <h1>posts</h1>
+      <div className="feed">
+        {posts.map((post) => (
+          <div className="post">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            <p className="small create-date"></p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default feed;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { useContext } from "react";
+import { postsContext } from "../Context/PostsContext";
 
 const Main = () => {
-  return <NavBar />;
+  const [Posts, setPosts] = useContext(postsContext);
+  return <NavBar Posts={Posts} />;
 };
 
 export default Main;
